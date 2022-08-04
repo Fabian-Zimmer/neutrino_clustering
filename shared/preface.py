9bdc9da93ec06565ac8865618d16e207d0c929a9
Fabian Zimmer FPC <f.zimmer@uva.nl>
2022-08-04 12:26:46 +0200

    s

--- shared/preface.py
+++ shared/preface.py
@@ -54,8 +54,8 @@ SEED = 0
 ### Local Folders ###
 #####################
 #! Uncomment correct path.
-SIM_DATA = '/home/fabian/ownCloud/sim_data'  # for UvA PC
-# SIM_DATA = '/home/fpc/SURFdrive/sim_data'  # for laptop
+# SIM_DATA = '/home/fabian/ownCloud/sim_data'  # for UvA PC
+SIM_DATA = '/home/fpc/SURFdrive/sim_data'  # for laptop
 
 
 
