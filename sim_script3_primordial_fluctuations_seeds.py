cc75a114b41f389db6868b3f3778d3e3c0fddba9
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2024-03-25 14:37:35 +0100

    seeds scripts

---
+++ sim_script3_primordial_fluctuations_seeds.py
@@ -0,0 +1,112 @@
+from Shared.specific_CNB_sim import *
+
+# This script solely computes the pixel and total densities,
+# when including the primordial temperature fluctuations, 
+# drawn from different seeds.
+
+# Argparse inputs.
+parser = argparse.ArgumentParser()
+parser.add_argument('--sim_dir', required=True)
+parser.add_argument('--shared_dir', required=True)
+pars = parser.parse_args()
+
+# Load simulation parameters
+simdata = SimData(pars.sim_dir)
+
+nu_m_picks = jnp.array([0.01, 0.05, 0.1, 0.2, 0.3])*Params.eV
+m_num = len(nu_m_picks)
+seeds = jnp.arange(10)
+
+### ============= ###
+### Create Deltas ###
+### ============= ###
+
+
+Cl_folder = f"Shared/Cls"
+Delta_folder = f"Shared/Deltas"
+
+
+_, Deltas_z4_matrix = SimUtil.generate_DeltaTs_seeds(
+    m_arr=nu_m_picks,
+    Delta_dir=Delta_folder,
+    seeds=seeds,
+    simdata=simdata,
+    args=Params())
+
+
+# Loop over all seeds for one fixed halo
+Deltas_seeds_l = []
+pix_dens_incl_PFs_seeds_l = []
+tot_dens_incl_PFs_seeds_l = []
+for i, _ in enumerate(seeds):
+
+    # Load neutrino vectors from simulation, for most massive halo
+    nu_vectors = jnp.load(f'{pars.sim_dir}/vectors_halo1.npy')
+
+    # Convert to momenta
+    v_arr = nu_vectors[..., 3:]
+    p_arr, y_arr = Physics.velocities_to_momenta_all_sky(
+        v_arr, nu_m_picks, Params())
+    # (masses, Npix, neutrinos per pixel, 2 (first and last time step))
+
+    # Momenta at z=0 and z=4
+    p_z0 = p_arr[..., 0]
+    p_z4 = p_arr[...,-1]
+    # (masses, Npix, neutrinos per pixel)
+
+    # Cl momenta are expressed in terms of T_CNB(z=0), but for proper matching 
+    # we need momenta at z=4 in terms of T_CNB(z=4).
+    q_z4 = y_arr[...,-1]/(1+4)
+    # (masses, Npix, neutrinos per pixel)
+
+    # Find indices to match neutrino momenta to Cl momenta
+    q_idx = jnp.abs(Primordial.Cl_qs[None,None,None,:] - q_z4[...,None]).argmin(axis=-1)
+    q_idx = jnp.reshape(q_idx, (m_num, -1))
+    # (masses, Npix, neutrinos per pixel)
+
+    # Pixel indices for all neutrinos
+    # (looks like [0,...,0,1,...,1,...,Npix-1,...,Npix-1])
+    p_idx = jnp.repeat(jnp.arange(simdata.Npix), simdata.p_num)[None, :]
+
+    # Mass indices adjusted for broadcasting / fancy indexing of Delta matrix
+    m_idx = jnp.arange(m_num)[:, None]
+
+    # Choose submatrix for current seed
+    Deltas_z4_seed = Deltas_z4_matrix[i]
+
+    # Select corresponding pixels, i.e. temp. perturbations, for all neutrinos
+    Deltas_halo = jnp.reshape(
+        Deltas_z4_seed[m_idx, q_idx, p_idx], 
+        (m_num, simdata.Npix, simdata.p_num))
+    Deltas_seeds_l.append(Deltas_halo)
+    # (masses, Npix, neutrinos per pixel)
+
+
+    ### ------------------------------------------------------------------ ###
+    ### Compute number densities incl. primordial temperature fluctuations ###
+    ### ------------------------------------------------------------------ ###
+
+    # For individual allsky map pixels
+    pix_dens_halo = Physics.number_density_Delta(
+        p_z0, p_z4, Deltas_halo, simdata.pix_sr, Params())
+    pix_dens_incl_PFs_seeds_l.append(pix_dens_halo)
+    # (masses, Npix)
+
+    # For total local value
+    tot_dens_halo = Physics.number_density_Delta(
+        p_z0.reshape(m_num, -1), 
+        p_z4.reshape(m_num, -1), 
+        Deltas_halo.reshape(m_num, -1), 
+        4*Params.Pi, Params())
+    tot_dens_incl_PFs_seeds_l.append(tot_dens_halo)
+
+
+jnp.save(
+    f"{pars.sim_dir}/Deltas_seeds.npy", jnp.array(Deltas_seeds_l))
+jnp.save(
+    f"{pars.sim_dir}/pixel_densities_incl_PFs_seeds", jnp.array(pix_dens_incl_PFs_seeds_l))
+jnp.save(
+    f"{pars.sim_dir}/total_densities_incl_PFs_seeds", jnp.array(tot_dens_incl_PFs_seeds_l))
+
+
+# note: then can download to local repo and make skymaps, power spectra, etc.
