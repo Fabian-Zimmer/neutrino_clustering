166158364634f69534759ca12bf2b29a5aa9b621
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2024-03-29 14:10:11 +0100

    v

--- sim_script2_execute_no_gravity.py
+++ sim_script2_execute_no_gravity.py
@@ -10,9 +10,6 @@ decayed_neutrinos_z = jnp.load(f'sim_output/no_gravity/decayed_neutrinos_z.npy')
 z_array = jnp.load(f'sim_output/no_gravity/z_int_steps.npy')
 neutrino_momenta = jnp.load(f'sim_output/no_gravity/neutrino_momenta.npy')
 
-m_l = 0.05 
-m_l = 0.3 
-m_phi = 0.000001
 # Argparse inputs.
 parser = argparse.ArgumentParser()
 parser.add_argument('--directory', required=True)
@@ -36,35 +33,38 @@ def find_nearest(array, value):
     return array[idx]
 
 
-#@jax.jit
+@jax.jit
 #specify mass as an argument too
-def EOMs_no_gravity(s_val, y, Nr):
-    Nr_index = Nr
-
+def EOMs_no_gravity(s_val, y, args):
+    Nr_index, angle_momentum_decay,decayed_neutrinos_z, z_array, neutrino_momenta = args
+    _, u_i_p = y
     # Find z corresponding to s via interpolation.
     z =  Utils.jax_interpolate(s_val, s_int_steps, z_int_steps)
     z_nearest = find_nearest(z_array, z)
 
    # check index of z in our z array
     z_index = jnp.argmax(jnp.equal(z_nearest, z_array)) # Need to access the first element of the array returned by where function
-    neutrino_number = jnp.int16(decayed_neutrinos_z[z_index,Nr_index][0])
-    prev_neutrino_number = jnp.int16(decayed_neutrinos_z[z_index - 1,Nr_index][0])
+    neutrino_number = jnp.int16(decayed_neutrinos_z[z_index,Nr_index])
+    prev_neutrino_number = jnp.int16(decayed_neutrinos_z[z_index - 1,Nr_index])
+    #if z_index>0:
+    #   prev_neutrino_number = jnp.int16(decayed_neutrinos_z[z_index - 1,Nr_index][0])
+   # else:
+    #    prev_neutrino_number = 1
     
  
     def true_func(y):
-        true_Nr.append(Nr_index)  
-        _, u_i_p = y
-        p_i = find_nearest(neutrino_momenta, 0.06 * jnp.linalg.norm(u_i_p, axis=-1)) #neutrino momenta pass as arg 
-        p_index = jnp.argmax(jnp.equal(neutrino_momenta, p_i))  # Need to access the first element of the array returned by where function
+       
+        p_i = find_nearest(neutrino_momenta, 0.06 * jnp.linalg.norm(y[1], axis=-1)) #neutrino momenta pass as arg 
+        p_index = jnp.argmax(jnp.equal(neutrino_momenta, p_i))  # Check which index it corresponds to
         
         angle_decay_theta = random.randint(jax.random.PRNGKey(0),(1,), 0, 179)
         angle_decay_phi = random.randint(jax.random.PRNGKey(0),(1,), 0, 360)
 
         momentum_daughter = angle_momentum_decay[angle_decay_theta,p_index][0]
-        
+      
         u_i = jnp.array( [(1 / 0.05) * momentum_daughter * jnp.sin(angle_decay_theta) * jnp.cos(angle_decay_phi),
             (1 / 0.05) * momentum_daughter * jnp.sin(angle_decay_theta) * jnp.sin(angle_decay_phi),
-            (1 / 0.05) * momentum_daughter * jnp.cos(angle_decay_theta)])
+            (1 / 0.05) * momentum_daughter * jnp.cos(angle_decay_theta)]) #mass should be an argument eventually
 
         return jnp.squeeze(u_i)
 
@@ -80,8 +80,10 @@ def EOMs_no_gravity(s_val, y, Nr):
     return dyds
 
 
-#@jax.jit
-def backtrack_1_neutrino(init_vector, s_int_steps):
+@jax.jit
+def backtrack_1_neutrino(init_vector, s_int_steps,angle_momentum_decay,decayed_neutrinos_z,z_array,neutrino_momenta):
+
+    
 
     """
     Simulate trajectory of 1 neutrino. Input is 6-dim. vector containing starting positions and velocities of neutrino. Solves ODEs given by the EOMs function with an jax-accelerated integration routine, using the diffrax library. Output are the positions and velocities at each timestep, which was specified with diffrax.SaveAt. 
@@ -112,7 +114,7 @@ def backtrack_1_neutrino(init_vector, s_int_steps):
     sol = diffrax.diffeqsolve(
         term, solver, 
         t0=t0, t1=t1, dt0=dt0, y0=y0, max_steps=10000,
-        saveat=saveat, stepsize_controller=stepsize_controller, args=(int(Nr),))
+        saveat=saveat, stepsize_controller=stepsize_controller, args=(Nr.astype(int),angle_momentum_decay,decayed_neutrinos_z,z_array, neutrino_momenta))
     
     trajectory = sol.ys.reshape(100,6)
 
@@ -120,8 +122,7 @@ def backtrack_1_neutrino(init_vector, s_int_steps):
     return jnp.stack([trajectory[0], trajectory[-1]])
 
 
-#@jax.jit
-def simulate_neutrinos_1_pix(init_xyz, init_vels, s_int_steps):
+def simulate_neutrinos_1_pix(init_xyz, init_vels, s_int_steps, Nr_column, angle_momentum_decay,decayed_neutrinos_z,z_array,neutrino_momenta):
     """
     Function for the multiprocessing routine below, which simulates all neutrinos for 1 pixel on the healpix skymap.
     """
@@ -132,12 +133,12 @@ def simulate_neutrinos_1_pix(init_xyz, init_vels, s_int_steps):
     init_vectors_0 = jnp.array(
         [jnp.concatenate((init_xyz, init_vels[k])) for k in range(nus)])
 
-    Nr_column = jnp.arange(1000).reshape(-1, 1)
+    Nr_column_reshaped = jnp.expand_dims(Nr_column, axis=1)
     # Concatenate the additional column to the original array
-    init_vectors = jnp.hstack((init_vectors_0, Nr_column))
+    init_vectors = jnp.hstack((init_vectors_0, Nr_column_reshaped))
 
     trajectories = jnp.array([
-        backtrack_1_neutrino(vec, s_int_steps) for vec in init_vectors])
+        backtrack_1_neutrino(vec, s_int_steps, angle_momentum_decay,decayed_neutrinos_z,z_array,neutrino_momenta) for vec in init_vectors])
 
     return trajectories  # shape = (neutrinos, 2, 3)
 
@@ -145,7 +146,7 @@ def simulate_neutrinos_1_pix(init_xyz, init_vels, s_int_steps):
 # Lists for pixel and total number densities
 pix_dens_l = []
 tot_dens_l = []
-true_Nr =[]
+
 # File name ending
 end_str = f'halo1'
 
@@ -171,13 +172,13 @@ nu_per_pix = sim_setup["momentum_num"]  # Number of neutrinos per healpixel
 
 init_vels = np.load(f'{pars.directory}/initial_velocities.npy')  
 # shape = (Npix, neutrinos per pixel, 3)
-
+Nr_column= jnp.arange(768000).reshape(768, 1000)
 # Use ProcessPoolExecutor to distribute the simulations across processes:
 # 1 process (i.e. CPU) simulates all neutrinos for one healpixel.
 with ProcessPoolExecutor(CPUs_sim) as executor:
     futures = [
         executor.submit(
-            simulate_neutrinos_1_pix, init_xyz, init_vels[pixel], s_int_steps) for pixel in range(Npix)]
+            simulate_neutrinos_1_pix, init_xyz, init_vels[pixel], s_int_steps, Nr_column[pixel], angle_momentum_decay,decayed_neutrinos_z,z_array,neutrino_momenta) for pixel in range(Npix)]
     
     # Wait for all futures to complete and collect results in order
     nu_vectors = jnp.array([future.result() for future in futures])
