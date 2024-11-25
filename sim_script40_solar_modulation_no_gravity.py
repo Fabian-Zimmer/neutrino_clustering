12371c566f4809e5fbae33dabc1764d1bbf2d228
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2024-11-25 12:52:42 +0100

    s

--- sim_script40_solar_modulation_no_gravity.py
+++ sim_script40_solar_modulation_no_gravity.py
@@ -152,9 +152,7 @@ def simulate_neutrinos_1_pix(init_xyz, init_vels, common_args):
 
 # Load Sun's positions and velocity vectors (w.r.t. CNB==CMB) in Earth-GC frame
 # for 1 year
-# sun_positions, sun_velocities, _ = SimPlot.SunEarthGC_frame_coords(2024)
 sun_positions, sun_velocities, _ = SimPlot.SunEarthGC_frame_coords_posvel(2024)
-# sun_positions, sun_velocities, _ = SimPlot.SunEarthGC_frame_coords_circular(2024)
 sun_positions *= Params.AU
 sun_velocities *= Params.km/Params.s
 
