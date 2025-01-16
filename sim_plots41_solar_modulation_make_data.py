67078fac8bf15f1463db4da18e90a839b33c8ac7
Fabian Zimmer <fabiantje.zimmertje@gmail.com>
2025-01-16 11:38:06 +0100

    s

--- sim_plots41_solar_modulation_make_data.py
+++ sim_plots41_solar_modulation_make_data.py
@@ -11,9 +11,10 @@ def compute_psd_day(y_z4_days, y_z0_dm_sim, fd_vals_z0):
 def calc_CNB_density_days(
         days_vecs_dir: str,
         day_step: int,
-        use_gravity: bool = True,
+        with_DM_gravity: bool = True,
         interp_grav_psd: bool = True,
-        use_boosted_fd: bool = False,
+        Earth_frame: bool = False,
+        rel_vel: str = "CNB",
         bound: bool | None = None,
         integrate_pixels: bool = True,
         args = None):
@@ -26,27 +27,28 @@ def calc_CNB_density_days(
     percentages = []
 
     # Earth velocities in GC frame (includes solar system motion)
-    _, _, earth_v = SimUtil.SunEarthGC_frame_coords_posvel(2024)
-    earth_v *= (args.km/args.s)/(args.kpc/args.s)
+    _, _, earth_v = SimUtil.SunEarthGC_frame_coords_posvel(2024, rel_vel)
+    earth_v_unit = args.km/args.s
+    earth_v_mags = jnp.linalg.norm(earth_v, axis=-1)*earth_v_unit
 
     # Initialize DM simulation data if using gravity
-    if use_gravity:
+    if with_DM_gravity:
         halo_num = 1
         dm_sim_v = SimData.load_velocities(
             sim_dir=sim_folder, halo_num=halo_num)
         
         # Calculate initial momentum arrays
-        _, p_z0_dm, p_z4_dm, y_z0_dm, _ = Utils.sim_vels_to_sorted_z0z4(
+        _, _, p_z4_dm, y_z0_dm, _ = Utils.sim_vels_to_sorted_z0z4(
             dm_sim_v, 
             nu_m_picks, 
             merge_last_axes=False, 
             args=args
         )
-        
+
         fd_vals_z0 = Physics.Fermi_Dirac(p_z4_dm, args)
         
         if bound is not None:
-            x_earth = jnp.array([8.127, 0., 0.]) * args.kpc
+            x_earth = jnp.array([8.127, 0., 0.])*args.kpc
             p_esc, _ = SimUtil.get_p_esc(sim_folder, x_earth, nu_m_picks, args)
 
     for day in range(0, 365, day_step):
@@ -59,13 +61,14 @@ def calc_CNB_density_days(
         days.append(day+1)
 
         # Load and transform velocities to Earth frame
-        day_v = jnp.load(fpath)[..., 3:][None, ...]
-        day_v = SimUtil.CNB_to_Earth_frame_trafo(day_v, earth_v[day])
+        v_unit = args.kpc/args.s
+        day_v = jnp.load(fpath)[..., 3:][None, ...]*v_unit
+        # (halos, 768, 1000, 2, 3)
 
-        if use_gravity:
+        if with_DM_gravity:
             # Calculate momentum arrays for gravity simulation
             sort_idx, p_z0, p_z4, _, y_z4 = Utils.sim_vels_to_sorted_z0z4(
-                day_v, 
+                day_v/v_unit,  # functions expects kpc/s units 
                 nu_m_picks, 
                 merge_last_axes=False, 
                 args=args
@@ -105,45 +108,44 @@ def calc_CNB_density_days(
 
         else:
             # Non-gravitational calculation
-            _, p_vec_z0, p_vec_z4, p_z0, p_z4, _, _ = Utils.sim_vels_to_sorted_z0z4_vec(
-                day_v, 
+            # _, _, _, p_z0, p_z4, _, _ = Utils.sim_vels_to_sorted_z0z4_vec(
+            _, p_z0, p_z4, _, _ = Utils.sim_vels_to_sorted_z0z4(
+                day_v/v_unit,  # functions expects kpc/s units
                 nu_m_picks, 
                 merge_last_axes = not integrate_pixels, 
                 args=args
             )
-            
-            if use_boosted_fd:
-                psd = Physics.Fermi_Dirac_boosted(
-                    p_vec_z4, 
-                    earth_v[day]*(args.kpc/args.s), 
-                    args
-                )
-            else:
-                psd = Physics.Fermi_Dirac(p_z4, args)
-
+            # p_z0/z4: (halos, masses, 768000) or (halos, masses, 768, 1000)
+            # depending on merge_last_axes True or False
+            psd = Physics.Fermi_Dirac(p_z4, args)
             n_raw = trap(p_z0**3 * psd, jnp.log(p_z0), axis=-1)
 
         # Compute final density
         if integrate_pixels:
-            pix_sr = 4 * args.Pi / simdata.Npix
-            n_dens = pix_sr * args.g_nu / ((2 * args.Pi)**3) * n_raw / args.cm**-3
+            pix_sr = 4*args.Pi / simdata.Npix
+            n_dens = pix_sr*args.g_nu / ((2*args.Pi)**3) * n_raw / args.cm**-3
             n_total = jnp.sum(jnp.array(n_dens), axis=-1)
         else:
-            pix_sr = 4 * args.Pi
-            n_dens = pix_sr * args.g_nu / ((2 * args.Pi)**3) * n_raw / args.cm**-3
+            pix_sr = 4*args.Pi
+            n_dens = pix_sr*args.g_nu / ((2*args.Pi)**3) * n_raw / args.cm**-3
             n_total = jnp.array(n_dens)
+
+        # Number density as seen on Earth
+        if Earth_frame:
+            n_total *= jnp.sqrt(1 - earth_v_mags[day])
 
         densities.append(n_total)
 
     results = (jnp.array(days), jnp.array(densities))
-    if bound is not None and use_gravity:
+    if bound is not None and with_DM_gravity:
         results += (jnp.array(percentages),)
     
     return results
 
 
 # Set preliminaries
-sim_name = f"SunNoG"
+# sim_name = f"SunNoG"
+sim_name = f"SunMod_1k"
 sim_folder = f"sim_output/{sim_name}"
 fig_folder = f"figures_local/{sim_name}"
 nu_m_range = jnp.load(f"{sim_folder}/neutrino_massrange_eV.npy")
@@ -159,14 +161,30 @@ exclude_nums = jnp.array([
 ])
 halo_nums = [x for x in range(1, 31) if x not in exclude_nums]
 
-# Set configuration
-days_vecs_dir = f"{sim_folder}/NoSun_vectors"
-prefix_str = "NoSun"
-use_gravity = False
+# No gravity
+# days_vecs_dir = f"{sim_folder}/NoSun_vectors"
+# prefix_str = "NoSun"
+# with_DM_gravity = False
+# interp_grav_psd = False
+
+# With Sun but no DM gravity
+# days_vecs_dir = f"{sim_folder}/WithSun_vel_CNB_vectors"
+# prefix_str = "SunNoDM"
+# with_DM_gravity = False
+# interp_grav_psd = False
+
+# With Sun and DM gravity
+days_vecs_dir = f"{sim_folder}/WithSun_vel_MW_vectors"
+prefix_str = "Sun+DM"
+with_DM_gravity = True
 interp_grav_psd = True
+
 day_step = 24  # Ultimately we want to use 1 to have all days
-use_boosted_fd = False
+Earth_frame = True
 integrate_pixels = True
+
+# rel_vel = "CNB"
+rel_vel = "MW"
 bound = None
 # bound: Momentum boundary condition:
 #     None - Use full momentum range
@@ -177,9 +195,10 @@ bound = None
 days, densities, *extra = calc_CNB_density_days(
     days_vecs_dir=days_vecs_dir, 
     day_step=day_step,
-    use_gravity=use_gravity,
+    with_DM_gravity=with_DM_gravity,
     interp_grav_psd=interp_grav_psd,
-    use_boosted_fd=use_boosted_fd,
+    Earth_frame=Earth_frame,
+    rel_vel=rel_vel,
     bound=bound,
     integrate_pixels=integrate_pixels,
     args=Params()
@@ -188,20 +207,25 @@ days, densities, *extra = calc_CNB_density_days(
 
 # Build filename suffixes based on conditions
 suffixes = []
-if use_gravity:
+if with_DM_gravity:
     if interp_grav_psd:
         suffixes.append('grav_PSD')
     else:
         suffixes.append('FD_PSD')
-if not use_gravity:
+if not with_DM_gravity:
     suffixes.append('FD_PSD')
-if use_boosted_fd:
-    suffixes.append('boosted')
+if Earth_frame:
+    if rel_vel == "CNB":
+        suffixes.append('Earth_frame_wrtCNB')
+    if rel_vel == "MW":
+        suffixes.append('Earth_frame_wrtMW')
 if bound is not None:
     suffixes.append('bound' if bound else 'unbound')
 if integrate_pixels:
     suffixes.append('int_pixels')
 suffix_str = f"_{'_'.join(suffixes)}" if suffixes else ""
+
+print(f"Done: {prefix_str}{suffix_str}")
 
 # Save arrays with prefix and suffix
 jnp.save(f"{sim_folder}/{prefix_str}_days_nums{suffix_str}.npy", days)
